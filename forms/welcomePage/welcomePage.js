fa10e624fbcf3669ca32a533184823155a74b02c
shaiannesunagawa <sys74530@creighton.edu>
2021-04-19 15:54:13 -0500

    working welcome page SS

--- forms/welcomePage/welcomePage.js
+++ forms/welcomePage/welcomePage.js
@@ -1,1 +1,20 @@
-//this is our first form 
+hmbrNav.onclick=function(s){
+  if (typeof(s) == "object") { // do nothing - they just clicked on the control
+       return
+    } else {
+       switch(s) {
+            case "New User":
+                ChangeForm(savesData)
+                break
+            case "Yelp":
+                ChangeForm(Yelp)
+                break
+            case "Map":
+                ChangeForm(CUMap)
+                break
+            case "Event Calendar":
+                ChangeForm(interactiveCalander)
+                break
+       } 
+   } 
+}
